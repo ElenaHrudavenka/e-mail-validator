91e6d76a9fe044e59be4b330a470cce1bbcaf317
ElenaHrudavenka <kisseli@mail.ru>
2022-11-09 18:18:55 +0300

    Update InputContainer.tsx

--- src/components/InputContainer.tsx
+++ src/components/InputContainer.tsx
@@ -2,59 +2,77 @@ import React, {ChangeEvent, useState} from 'react';
 import s from './InputContainer.module.css'
 
 const InputContainer = () => {
-    const [value, setValue] = useState('');
+    const [valueInput, setValueInput] = useState('');
     const [inputStyle, setInputStyle] = useState<number>(0);
 
-    // функция валидации e-mail
+    // функция валидации e-mail (вернет true если значение прошло валидацию)
     const isValidateEmail = (email: string): boolean => {
-        let patternEmail = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-        return patternEmail.test(email.toLowerCase())
-    }
+        const patternEmail = /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/iu;
+        return patternEmail.test(email)
+    };
 
-    // дефисы в домене
-    const containDash = (hostName: string) => {
-        let domain = hostName.substring(0, hostName.indexOf('.'))
-        let lastCharacter = domain.length-1;
-        return domain[0] === '-' || domain[lastCharacter] === '-'
-    }
+    // дефисы в домене (вернет true если дефис содржится в начале и конце строки или идет более двух подряд)
+    const isContainDash = (hosts: Array<string>) => {
+        const patternDomain = /^(?![-]|.*[-]$)(?=[^-]*[-]?[^-]*$).{1,63}$/iu;
+        let i = 0, contain = false;
+        while (i < hosts.length) {
+            if (!patternDomain.test(hosts[i])) {
+                contain = true;
+                break
+            }
+            i++;
+        }
+        return contain;
+    };
+// доменная зона (true если есть ошибка валидации)
+    const isErrorZone = (hostZone: string): boolean => {
+        const patternZone = /(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])/iu;
+        //alert(`host: ${hostZone} result: ${patternZone.test(hostZone)}`)
+        return !patternZone.test(hostZone);
+    };
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setValue(e.currentTarget.value);
-    }
+        setValueInput(e.currentTarget.value);
+    };
 
     const onBlurHandler = () => {
-        let email: string = value.trim();
-        const [userName, hostName]: Array<string> = email.split('@');
-        if (containDash(hostName)) {
-            setInputStyle(4)
-        } else if (!isValidateEmail(email)) {
-            !userName && setInputStyle(2);
-            !hostName && setInputStyle(3);
-            hostName && !hostName.includes('.') && setInputStyle(5);
-        } else {
+        let email: string = valueInput.trim();
+        const [userName, hostName] = email.includes('@') ? email.split('@') : [email, ''];
+        const hosts: Array<string> = hostName.split('.');
+        !userName && setInputStyle(2);
+        !hostName.includes('.') && setInputStyle(3);
+        if (isValidateEmail(email)) {
             setInputStyle(1);
+        } else {
+           isErrorZone(hosts[hosts.length - 1]) && setInputStyle(5);
+           hosts[hosts.length - 1]
+               ? isContainDash(hosts.slice(0,-1)) && setInputStyle(4)
+               : isContainDash(hosts) &&setInputStyle(4);
         }
-    }
+    };
 
     const onFocusHandler = () => {
         setInputStyle(0);
-    }
+    };
 
     return (
         <div className={s.inputBlock}>
-            <input value={value}
+            <input value={valueInput}
                    type="text"
                    placeholder='input your e-mail'
-                   className={inputStyle > 1 ? s.validationError : s.validationSuccess}
+                   className={inputStyle > 1
+                       ? s.validationError
+                       : s.validationSuccess}
                    onChange={onChangeHandler}
                    onBlur={onBlurHandler}
                    onFocus={onFocusHandler}
             />
             {inputStyle === 1 && <span data-descr-success={"Your e-mail is received!"}> </span>}
             {inputStyle === 2 && <span data-descr={"User name not entered!"}> </span>}
-            {inputStyle === 3 && <span data-descr={"Domain name not entered!"}> </span>}
-            {inputStyle === 4 && <span data-descr={"Don't use dash at the beginning or end of a domain!"}> </span>}
-            {inputStyle === 5 && <span data-descr={"Domain zone not entered!"}> </span>}
+            {inputStyle === 3 && <span data-descr={"Host name must contain domain name and zone!"}> </span>}
+            {inputStyle === 4 && <span
+                data-descr={"Don't use dash at the beginning or end of your domain name! Don't use more than one dash in a row!"}> </span>}
+            {inputStyle === 5 && <span data-descr={"Domain zone not entered or consist errors!"}> </span>}
         </div>
     );
 };
