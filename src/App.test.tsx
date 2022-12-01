739e80c076e8b12d1f663d9fdf62ee4b1159ba97
ElenaHrudavenka <kisseli@mail.ru>
2022-12-01 15:49:11 +0300

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import App from './App';
 import StatusDescription from "./components/StatusDescription";
+import userEvent from "@testing-library/user-event";
 
 test('Renders input title.', () => {
     render(<App/>);
@@ -18,9 +19,12 @@ test('Renders input element.', () => {
 describe('StatusDescription', () => {
     test('Renders StatusDescription element.', () => {
         render(<App/>);
-        const inputElement = screen.getByRole<HTMLInputElement>('');
-        expect(inputElement).toBeInTheDocument();
+        userEvent.type(screen.getByRole<HTMLInputElement>('textbox'), 'kisseli@mail.ru');
+        userEvent.tab();
+        const descriptionBlock = document.getElementsByClassName('descriptionBlock')
+        expect(descriptionBlock[0].children[0]).toContainHTML('<span data-descr-');
     });
+
     test('No success message by default.',  () => {
         render(<App/>);
         expect(screen.queryByTestId('success')).not.toBeInTheDocument();
